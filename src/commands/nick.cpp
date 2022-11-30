655032104b7ecff0d4bd993cd20486bc489b71ae
ostef <47391841+ostef@users.noreply.github.com>
2022-11-30 18:30:51 +0100

    fix: reply nick after changing the nickname

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -52,9 +52,9 @@ void Server::nick(User &u, const Message &msg)
 		}
 	}
 
-	reply (u, msg);	// Make sure the user itself is notified of the nick change
 
 	u.nickname = nick;
+	reply (u, msg);	// Make sure the user itself is notified of the nick change
 	if (!wasRegistered && u.isRegistered ())
 		reply (u, Reply::welcome (u.nickname, u.username, u.getAddressAsString ()));
 }
